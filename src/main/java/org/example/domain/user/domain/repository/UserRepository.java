41e15d499b5b712fbd667bda4092b0062abf398a
handokei <ehgks994@gmail.com>
2025-12-12 15:46:07 +0900

    feat:  repository 생성

---
+++ src/main/java/org/example/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.example.domain.user.domain.repository;
+
+import org.example.domain.user.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmailAndIsDeletedFalse(String email);
+
+
+    Optional<User> findByEmailAndIsDeletedFalse(String email);
+
+    Optional<User> findByIdAndIsDeletedFalse(Long userId);
+}
