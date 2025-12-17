f2c4694034a3b06eedb745c37d20e4da612782b8
handokei <ehgks994@gmail.com>
2025-12-17 22:05:02 +0900

    refactor: userRole 추가에 따른 코드 수정

--- src/main/java/org/example/global/security/auth/CustomUserDetailsService.java
+++ src/main/java/org/example/global/security/auth/CustomUserDetailsService.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.domain.user.domain.model.User;
 import org.example.domain.user.domain.repository.UserRepository;
 import org.example.global.security.jwt.CustomUserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +20,15 @@ public class CustomUserDetailsService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
+        String roleName = "ROLE_" + user.getUserRole().name();
+
         return new CustomUserDetails(
                 user.getId(),
                 user.getName(),
                 user.getNickname(),
                 user.getEmail(),
-                user.getPassword(),
-                null
+                user.getPassword()
+                , List.of(new SimpleGrantedAuthority(roleName))
         );
     }
 }
