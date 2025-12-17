dbac3cae485824ec74b78d27d5b47eeae1c063e1
handokei <ehgks994@gmail.com>
2025-12-17 22:05:41 +0900

    refactor: 401,403에러 구분, 인증코드 추가 수정

--- src/main/java/org/example/global/config/SecurityConfig.java
+++ src/main/java/org/example/global/config/SecurityConfig.java
@@ -1,16 +1,71 @@
 package org.example.global.config;
 
+import org.example.global.security.auth.CustomUserDetailsService;
+import org.example.global.security.jwt.JwtAuthenticationFilter;
+import org.example.global.security.jwt.JwtProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtProvider jwtProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(JwtProvider jwtProvider, CustomUserDetailsService customUserDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtProvider,customUserDetailsService);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                // CSRF 비활성화 (REST API / Postman 테스트용)
+                .csrf(csrf -> csrf.disable())
+
+                // 인가 설정
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/api/users/register",
+                                "/api/users/login",
+                                "/api/products/**"
+                        ).permitAll()
+
+                        .requestMatchers("/api/admin/**")
+                        .hasRole("ADMIN")
+
+
+                        .anyRequest().authenticated()
+
+                )
+
+                .addFilterBefore(jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+
+
+                // ===== 나머지 ====
+
+
+        return http.build();
+    }
+
+    // 비밀번호 암호화
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
