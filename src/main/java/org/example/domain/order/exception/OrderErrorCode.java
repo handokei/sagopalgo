03430b437db1b3b9fe4e780697ac1ea46ed86b1e
handokei <ehgks994@gmail.com>
2025-12-18 23:39:48 +0900

    feat: OrderException 예외처리 생성

---
+++ src/main/java/org/example/domain/order/exception/OrderErrorCode.java
@@ -0,0 +1,21 @@
+package org.example.domain.order.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.global.response.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum OrderErrorCode implements ErrorCode {
+    ORDER_NOT_FOUND_EXCEPTION(HttpStatus.UNAUTHORIZED,"존재하지 않는 주문입니다."),
+    ORDER_STATUS_NOT_CREATED(HttpStatus.UNAUTHORIZED,"주문 상태가 생성상태가 아닙니다." ),
+    ORDER_ALREADY_COMPLETED(HttpStatus.UNAUTHORIZED,"주문완료 상태여서 취소가 불가능합니다." ),
+    ORDER_ALREADY_CANCELED(HttpStatus.UNAUTHORIZED,"이미 주문 취소한 주문입니다." ),
+    ORDER_STATUS_NOT_PAID(HttpStatus.UNAUTHORIZED, "주문 상태가 결제 완료 상태가 아닙니다." ),
+    ORDER_STATUS_NOT_SHIPPED(HttpStatus.UNAUTHORIZED, "주문 상태가 배송중이 아닙니다." );
+
+
+private final HttpStatus status;
+private final String message;
+}
