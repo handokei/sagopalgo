2e0c9f9e9544a3176e76336b627db31f22a3adbd
handokei <ehgks994@gmail.com>
2025-12-18 23:40:51 +0900

    feat: Dto 설정

---
+++ src/main/java/org/example/domain/order/controller/dto/OrderResponseDto.java
@@ -0,0 +1,32 @@
+package org.example.domain.order.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.domain.order.domain.model.Order;
+import org.example.domain.order.domain.model.OrderStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class OrderResponseDto {
+
+    private Long id;
+
+    private String productTitle;
+
+    private int totalPrice;
+
+    private OrderStatus orderStatus;
+
+    private LocalDateTime createAt;
+
+
+    public static OrderResponseDto from(Order order) {
+        return new OrderResponseDto(order.getId(),
+                order.getSummaryTitle(),
+                        order.getTotalPrice(),
+                        order.getOrderStatus(),
+                        order.getCreatedAt());
+    }
+}
