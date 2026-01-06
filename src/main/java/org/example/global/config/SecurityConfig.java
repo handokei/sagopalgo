d6f0a62ff9902f297cd16f538538d7e0e5b7abf3
handokei <ehgks994@gmail.com>
2026-01-06 12:30:47 +0900

    refactor: 세션 및 기본 인증 비활성화로 JWT 인증 방식 적용

--- src/main/java/org/example/global/config/SecurityConfig.java
+++ src/main/java/org/example/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.global.security.auth.CustomUserDetailsService;
 import org.example.global.security.jwt.JwtAuthenticationFilter;
 import org.example.global.security.jwt.JwtProvider;
@@ -8,65 +9,50 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final CustomUserDetailsService customUserDetailsService;
 
-    public SecurityConfig(JwtProvider jwtProvider, CustomUserDetailsService customUserDetailsService) {
-        this.jwtProvider = jwtProvider;
-        this.customUserDetailsService = customUserDetailsService;
-    }
-
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtProvider,customUserDetailsService);
+        return new JwtAuthenticationFilter(jwtProvider, customUserDetailsService);
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                // CSRF 비활성화 (REST API / Postman 테스트용)
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
 
-                // 인가 설정
                 .authorizeHttpRequests(auth -> auth
-
                         .requestMatchers(
                                 "/api/users/register",
                                 "/api/users/login",
-                                "/api/products/**",
                                 "/api/carts/**"
                         ).permitAll()
-
-                        .requestMatchers("/api/admin/**")
-                        .hasRole("ADMIN")
-
-
+                        .requestMatchers("/api/orders/**").authenticated()
                         .anyRequest().authenticated()
-
                 )
 
-                .addFilterBefore(jwtAuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
-
-
-                // ===== 나머지 ====
-
+                .addFilterBefore(
+                        jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
-    }
-
-    // 비밀번호 암호화
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
