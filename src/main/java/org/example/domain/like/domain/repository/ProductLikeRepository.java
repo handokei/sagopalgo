5a75dbeb0aa1e372ba752cc3c114af68339dd866
handokei <ehgks994@gmail.com>
2026-01-08 21:08:09 +0900

    feat: 조회 로직 생성
    refactor: boolean값으로 토글 형식처럼 작동 될 수 있도록 로직 재수정

--- src/main/java/org/example/domain/like/domain/repository/ProductLikeRepository.java
+++ src/main/java/org/example/domain/like/domain/repository/ProductLikeRepository.java
@@ -1,6 +1,8 @@
 package org.example.domain.like.domain.repository;
 
 import org.example.domain.like.domain.model.ProductLike;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
     Optional<ProductLike> findByProductIdAndUserId(Long id, Long userId);
+
+    Page<ProductLike> findAllByUserId(Long userId, PageRequest pageRequest);
 }
--- src/main/java/org/example/domain/like/service/ProductLikeService.java
+++ src/main/java/org/example/domain/like/service/ProductLikeService.java
@@ -1,6 +1,8 @@
 package org.example.domain.like.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.like.controller.dto.ProductLikeResponseDto;
+import org.example.domain.like.controller.dto.ProductLikesCreateResponseDto;
 import org.example.domain.like.domain.model.ProductLike;
 import org.example.domain.like.domain.repository.ProductLikeRepository;
 import org.example.domain.product.domain.repository.ProductRepository;
@@ -9,6 +11,8 @@ import org.example.domain.product.exception.ProductException;
 import org.example.domain.user.domain.repository.UserRepository;
 import org.example.domain.user.exception.UserErrorCode;
 import org.example.domain.user.exception.UserException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -21,7 +25,7 @@ public class ProductLikeService {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
-    public void createLike(Long id, Long userId) {
+    public ProductLikesCreateResponseDto createLike(Long id, Long userId) {
         productRepository.findByIdAndIsDeletedFalse(id)
                 .orElseThrow(() -> new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND_EXCEPTION));
 
@@ -32,8 +36,23 @@ public class ProductLikeService {
 
         if (like.isPresent()) {
             productLikeRepository.delete(like.get());
+            return ProductLikesCreateResponseDto.from(false);
         } else {
-            productLikeRepository.save(ProductLike.of(id,userId));
+            productLikeRepository.save(ProductLike.of(userId,id));
         }
+        return ProductLikesCreateResponseDto.from(true);
+    }
+
+    public Page<ProductLikeResponseDto> getProductLikes(Long userId, int page, int size) {
+
+        userRepository.findByIdAndIsDeletedFalse(userId)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND_EXCEPTION));
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+
+        return productLikeRepository
+                .findAllByUserId(userId, pageRequest)
+                .map(ProductLikeResponseDto::from);
     }
 }
