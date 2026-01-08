4e78c7f711d89771718aefc76943d312e49cb871
handokei <ehgks994@gmail.com>
2026-01-08 17:35:57 +0900

    feat: 3계층으로 분리하여 설계

---
+++ src/main/java/org/example/domain/like/domain/repository/ProductLikeRepository.java
@@ -0,0 +1,4 @@
+package org.example.domain.like.domain.repository;
+
+public interface ProductLikeRepository {
+}
---
+++ src/main/java/org/example/domain/like/service/ProductLikeService.java
@@ -0,0 +1,4 @@
+package org.example.domain.like.service;
+
+public class ProductLikeService {
+}
