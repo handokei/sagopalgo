5bcd1a0625308ed75df1825a7d00598940404aa2
handokei <ehgks994@gmail.com>
2026-01-09 23:36:39 +0900

    feat: Page출력을 위해 pageable,total(count)함께 return으로 수정

--- src/main/java/org/example/domain/product/domain/repository/ProductCustomRepositoryImpl.java
+++ src/main/java/org/example/domain/product/domain/repository/ProductCustomRepositoryImpl.java
@@ -4,9 +4,9 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.like.domain.model.QProductLike;
-import org.example.domain.order.domain.model.QOrder;
 import org.example.domain.order.domain.model.QOrderItem;
 import org.example.domain.product.domain.model.Product;
+import org.example.domain.product.domain.model.ProductCategory;
 import org.example.domain.product.domain.model.QProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -22,7 +22,7 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<Product> search(Pageable pageable, String sort) {
+    public Page<Product> search(Pageable pageable, String sort, ProductCategory productCategory) {
         QProduct product = QProduct.product;
         QProductLike productLike = QProductLike.productLike;
         QOrderItem orderItem = QOrderItem.orderItem;
@@ -31,21 +31,33 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
         /**
          * 통합: 좋아요수 , 주문수 , 가격 높은 순 낮은 순
          */
-        List<Product> result = queryFactory.selectFrom(product)
-                .leftJoin(productLike).on(productLike.id.eq(product.id))
+        List<Product> result = queryFactory
+                .selectFrom(product)
+                .leftJoin(productLike).on(productLike.productId.eq(product.id))
                 .leftJoin(orderItem).on(orderItem.productId.eq(product.id))
+                .where(product.productCategory.eq(productCategory))
                 .groupBy(product.id)
                 .orderBy(
-                        productLike.count().desc(),
-                        orderItem.count().desc(),
-                        product.price.desc()
+                        getOrderSpecifier(sort,
+                                product,
+                                productLike,
+                                orderItem)
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        return new PageImpl<>(result);
+        //count
+        Long total = queryFactory
+                .select(product.count())
+                .from(product)
+                .where(product.productCategory.eq(productCategory))
+                .fetchOne();
+        return new PageImpl<>(result,pageable, total);
+
     }
+
+
 
     private OrderSpecifier<?> getOrderSpecifier(String sort, QProduct product, QProductLike productLike, QOrderItem orderItem){
         if (sort == null) {
