6634079c8d09106cafb405d55736edf8b3ba3f3d
handokei <ehgks994@gmail.com>
2026-02-18 02:12:16 +0900

    refactor: 재고 노출을 없애기 위해 반환값 수정

--- src/main/java/org/example/domain/product/controller/dto/ProductResponseDto.java
+++ src/main/java/org/example/domain/product/controller/dto/ProductResponseDto.java
@@ -22,8 +22,6 @@ public class ProductResponseDto {
 
     private int price;
 
-    private int stock;
-
     private ProductStatus productStatus;
 
     private ProductCategory productCategory;
@@ -37,7 +35,6 @@ public class ProductResponseDto {
         this.title = product.getTitle();
         this.contents = product.getContents();
         this.price = product.getPrice();
-        this.stock = product.getStock();
         this.productStatus = product.getProductStatus();
         this.productCategory = product.getProductCategory();
         this.createdAt = product.getCreatedAt();
