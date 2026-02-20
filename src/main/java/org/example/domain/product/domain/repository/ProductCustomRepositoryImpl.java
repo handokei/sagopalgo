1bee5ec9200f13b256ccfd4e92094cdbd213d25b
handokei <ehgks994@gmail.com>
2026-02-20 14:00:50 +0900

    refactor(product): Repository에 categoryId 필터링 적용

--- src/main/java/org/example/domain/product/domain/repository/ProductCustomRepositoryImpl.java
+++ src/main/java/org/example/domain/product/domain/repository/ProductCustomRepositoryImpl.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.domain.like.domain.model.QProductLike;
 import org.example.domain.order.domain.model.QOrderItem;
 import org.example.domain.product.domain.model.Product;
-import org.example.domain.product.domain.model.ProductCategory;
 import org.example.domain.product.domain.model.QProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -23,23 +22,20 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<Product> search(Pageable pageable, String sort, ProductCategory productCategory, String keyword) {
+    public Page<Product> search(Pageable pageable, String sort, Long categoryId, String keyword) {
         QProduct product = QProduct.product;
         QProductLike productLike = QProductLike.productLike;
         QOrderItem orderItem = QOrderItem.orderItem;
 
-
-        /**
-         * 통합: 좋아요수 , 주문수 , 가격 높은 순 낮은 순
-         */
         List<Product> result = queryFactory
                 .selectFrom(product)
                 .leftJoin(product.seller).fetchJoin()
+                .leftJoin(product.category).fetchJoin()
                 .leftJoin(productLike).on(productLike.productId.eq(product.id))
                 .leftJoin(orderItem).on(orderItem.productId.eq(product.id))
                 .where(product.isDeleted.eq(false),
-                        categoryEquals(productCategory,product),
-                        titleContains(keyword,product))
+                        categoryIdEquals(categoryId, product),
+                        titleContains(keyword, product))
                 .groupBy(product.id, product.seller.id)
                 .orderBy(
                         getOrderSpecifier(sort,
@@ -51,51 +47,41 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        //count
         Long total = queryFactory
                 .select(product.count())
                 .from(product)
                 .where(product.isDeleted.eq(false),
-                        categoryEquals(productCategory,product),
-                        titleContains(keyword,product))
+                        categoryIdEquals(categoryId, product),
+                        titleContains(keyword, product))
                 .fetchOne();
-        return new PageImpl<>(result,pageable, total);
 
+        return new PageImpl<>(result, pageable, total);
     }
 
-
-
-    private OrderSpecifier<?> getOrderSpecifier(String sort, QProduct product, QProductLike productLike, QOrderItem orderItem){
+    private OrderSpecifier<?> getOrderSpecifier(String sort, QProduct product, QProductLike productLike, QOrderItem orderItem) {
         if (sort == null) {
             return product.id.asc();
         }
 
-        return switch (sort){
+        return switch (sort) {
             case "price_desc" -> product.price.desc();
             case "price_asc" -> product.price.asc();
             case "likes_count" -> productLike.count().desc();
-            case "popular" ->  orderItem.count().desc();
+            case "popular" -> orderItem.count().desc();
             case "latest" -> product.createdAt.desc();
             default -> product.id.asc();
         };
     }
 
-    /**
-     * 검색
-     * 가격
-     */
-
-    private BooleanExpression titleContains(String keyword, QProduct product){
+    private BooleanExpression titleContains(String keyword, QProduct product) {
         return keyword == null || keyword.isBlank()
                 ? null
                 : product.title.contains(keyword);
     }
-    private BooleanExpression categoryEquals(ProductCategory category,
-                                             QProduct product) {
-        return category == null
-                ? null
-                : product.productCategory.eq(category);
-    }
 
-    //
+    private BooleanExpression categoryIdEquals(Long categoryId, QProduct product) {
+        return categoryId == null
+                ? null
+                : product.categoryId.eq(categoryId);
+    }
 }
