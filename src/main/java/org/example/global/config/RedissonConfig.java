3498d6116ec5e9dfb643efb10044c0b6a2f7f6cf
handokei <ehgks994@gmail.com>
2026-02-23 20:40:47 +0900

    fix: RedissonConfig SSL 연결로 변경 (rediss://)

--- src/main/java/org/example/global/config/RedissonConfig.java
+++ src/main/java/org/example/global/config/RedissonConfig.java
@@ -20,7 +20,7 @@ public class RedissonConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setAddress("rediss://" + redisHost + ":" + redisPort)
                 .setConnectionMinimumIdleSize(1)
                 .setConnectionPoolSize(2);
         return Redisson.create(config);
